8819a82bbe0aedad61e838be86fc434cd3f0ec7d
Bego2 <beck28cz@alumnos.cei.es>
2025-01-16 18:40:15 +0100

    updates - varios

--- js/projects.js
+++ js/projects.js
@@ -1,27 +1,40 @@
-const proyectos = [
+/* PROYECTOS pt1.  En este documento he trabajado la parte de código que genera cada uno de los proyectos de mi portfolio. 
+
+1. Declaramos la variable "proyectos", que guarda un array con 4 objetos, esos objetos son los proyectos. Cada proyecto comparte propiedades, pero para cada una de ellas guardamos diferentes valores. 
+
+2. Creamos la funcion que mostrará en pantalla el proyecto que corresponda.
+
+3. Seleccionamos el id= "proyecto" que se encuentra en el html de projects.html dentro de la etiqueta main, pues será aquí dentro donde gracias al innerHTML se irá mostrando el contenido.
+
+4. Se localiza la parte de la url que va despues de la ? y guarda los parametros en la constante urlParams. Nos servirá para el punto 5. 
+
+5. De urlParams se obtiene el id de la url, gracias al metodo get. Esto es útil porque después buscará el id de un proyecto para mostrarlo 
+
+6. Aquí es donde se busca dentro del array el primer proyecto cuyo id coincida con el id obtenido de la url, para despues mostrarlo
+
+Pd. hay una conexión html-javascript. Desde el index html clickando en un proyecto nos deriva a su pagina correspondiente gracias a href= "projects.html?id=[x]" */
+
+//1
+let proyectos = [
   {
     id: 1,
     icono: "./img/steez-iconotitulo.svg",
     titulo: "Steez",
     alt: "icono de steez, carita sonriente en negro sobre fondo neon",
-    descripcion:
-      "Identidad gráfica y diseño de interfaz para una aplicación de intercambio y alquiler de sneakers.Exchange & Shine se convierte en el lema de personas jóvenes que aman la moda, las sneakers y la sostenibilidad.",
+    descripcion: "Identidad gráfica y diseño de interfaz para una aplicación de intercambio y alquiler de sneakers. Exchange & Shine se convierte en el lema de personas jóvenes que aman la moda, las sneakers y la sostenibilidad.",
 
-    heading1: "Llamando la atención a través de la identidad",
-    text1:
-      "La identidad visual se conforma por una paleta de color de colores neutros, gris y negro con un acento de color neón. Las tipografías combinadas son sin serifa, pero contrastadas y desenfadadas.",
+    heading1:"Llamando la atención a través de la identidad",
+    text1: "La identidad visual se conforma por una paleta de color de colores neutros, gris y negro con un acento de color neón. Las tipografías combinadas son sin serifa, pero contrastadas y desenfadadas.",
     img1: "./img/steez-pantallasmobile.webp",
     alt1: "tres pantallas formato mobile, mostrando la identidad de steez",
 
-    heading2: "Iconos que no dejan indifente",
-    text2:
-      "Me encantó realizar el set de iconos. Realizados vectorlialmente con Illustrator. Iconos asimétricos y afilados que aportan a la identidad gráfica un toque canalla, llamativo y diferenciador.",
+    heading2:"Iconos que no dejan indifente",
+    text2:"Me encantó realizar el set de iconos. Realizados vectorlialmente con Illustrator. Iconos asimétricos y afilados que aportan a la identidad gráfica un toque canalla, llamativo y diferenciador.",
     img2: "./img/steez-iconos.webp",
     alt2: "iconos de steez y una pantalla móvil donde se muestran estos iconos aplicados",
 
     heading3: "La importancia de priorizar",
-    text3:
-      "Previamente a la realización de las pantallas estuvimos evaluando cómo podíamos realizar una aplicación atractiva para el público joven, sin que dejase de ser intuitiva y fácil de usar. Gracias a la pirámide UX y al árbol de funcionalidades logramos nuestro objetivo.",
+    text3: "Previamente a la realización de las pantallas estuvimos evaluando cómo podíamos realizar una aplicación atractiva para el público joven, sin que dejase de ser intuitiva y fácil de usar. Gracias a la pirámide UX y al árbol de funcionalidades logramos nuestro objetivo.",
     img3: "./img/steez-arbolfuncionalidades.webp",
     alt3: "árbol de funcionalidades de steez",
 
@@ -33,24 +46,20 @@ const proyectos = [
     icono: "./img/brillo-iconotitulo.svg",
     titulo: "Brillo",
     alt: "icono de brillo, una b sobre fondo verde oliva",
-    descripcion:
-      " Hubo talleres, rutas guiadas, conexión con el mundo rural y dinámicas de grupo. Retiro brillo tiene un tono de voz dulce y auténtico. Su territorio es la creatividad y sus valores la naturaleza, la luminosidad y la introspección.",
+    descripcion: "Hubo talleres, rutas guiadas, conexión con el mundo rural y dinámicas de grupo. Retiro brillo tiene un tono de voz dulce y auténtico. Su territorio es la creatividad y sus valores la naturaleza, la luminosidad y la introspección.",
 
     heading1: "Una dirección de arte sonriente",
-    text1:
-      " La identidad visual se conforma por los dos colores principales que refuerzan la idea de naturaleza. La paleta se estira y se unen otros colores a la web que evocan creatividad. Hay ilustraciones pintadas que sirven como iconos o imágenes de soporte. Hay fotografías coloridas hechas con luz natural.",
+    text1: "La identidad visual se conforma por los dos colores principales que refuerzan la idea de naturaleza. La paleta se estira y se unen otros colores a la web que evocan creatividad. Hay ilustraciones pintadas que sirven como iconos o imágenes de soporte. Hay fotografías coloridas hechas con luz natural.",
     img1: "./img/brillo-identidad.webp",
     alt1: "identidad visual del proyecto brillo, logo, fotografía, landing web",
 
     heading2: "Las redes sociales toman importancia",
-    text2:
-      "Cuidar los detalles y comunicar la información en redes sociales se volvió crucial para que el retiro llegase a más personas. Tanto las historias destacadas de Instagram como los reels son espacios donde la identidad de marca se cuida y se presenta, generando memorabilidad en el usuario.",
+    text2: "Cuidar los detalles y comunicar la información en redes sociales se volvió crucial para que el retiro llegase a más personas. Tanto las historias destacadas de Instagram como los reels son espacios donde la identidad de marca se cuida y se presenta, generando memorabilidad en el usuario.",
     img2: "./img/brillo-instagram.webp",
     alt2: "historias de instagram, se han aplicado los colores de marca y la identidad visual",
 
     heading3: "Algunas pruebas y decisiones",
-    text3:
-      "El lanzamiento de la web fue con Webflow, vinieron las pruebas, aquí algunas: logo, color, prototipos. En ese momento no tenía los conocimientos de ahora, por lo que hay muchos errores. Me alegro de haberlo intentado, gracias a este proyecto decidí formarme en diseño web :)",
+    text3: "El lanzamiento de la web fue con Webflow, vinieron las pruebas, aquí algunas: logo, color, prototipos. En ese momento no tenía los conocimientos de ahora, por lo que hay muchos errores. Me alegro de haberlo intentado, gracias a este proyecto decidí formarme en diseño web :)",
     img3: "./img/brillo-pruebas.webp",
     alt3: "pruebas de logo, color y prototipado web",
     imgaside: "./img/thumbnail-brillo.webp",
@@ -61,24 +70,20 @@ const proyectos = [
     icono: "./img/festival-iconotitulo.svg",
     titulo: "Item",
     alt: " ",
-    descripcion:
-      "Item es un festival de motion design para estudiantes y profesionales de esta área. Se ha construido la interfaz para la web, desde donde se gestiona la venta de entradas y se aporta información del evento. Mientras que la app es utilizada por los asistentes.",
+    descripcion: "Item es un festival de motion design para estudiantes y profesionales de esta área. Se ha construido la interfaz para la web, desde donde se gestiona la venta de entradas y se aporta información del evento. Mientras que la app es utilizada por los asistentes.",
 
     heading1: "Soluciones nicho para un nicho de diseñadores",
-    text1:
-      "La comunicación gráfica apela al público objetivo de Item: los motion designers. Para la gráfica de este festival, he tomado de referencia softwares de animación. Encontramos símbolos, jerarquías y un diseño de interacción inspirados en estos programas.",
+    text1: "La comunicación gráfica apela al público objetivo de Item: los motion designers. Para la gráfica de este festival, he tomado de referencia softwares de animación. Encontramos símbolos, jerarquías y un diseño de interacción inspirados en estos programas.",
     img1: "./img/festival-desktop.webp",
     alt1: "ahoa ",
 
     heading2: "Dispositivos diferentes, objetivos distintos",
-    text2:
-      "El festival tiene una web donde acceder a la programa, mapa del recinto y otra información. Mientras que la app sirve a los participantes para acceder al evento, revisar a las actividades donde se han inscrito o poder inscribirse en nuevas.",
+    text2: "El festival tiene una web donde acceder a la programa, mapa del recinto y otra información. Mientras que la app sirve a los participantes para acceder al evento, revisar a las actividades donde se han inscrito o poder inscribirse en nuevas.",
     img2: "./img/festival-dispositivos.webp",
     alt2: "holaa ",
 
     heading3: "Diseño centrado en el usuario",
-    text3:
-      "Un estudio previo de las personas que asistirían al festival fue clave para averiguar qué tipo de propuesta realizar, tanto en web como en app. Pues las necesidades del usuario en los diferentes dispositivos era diferente. Es por eso que se identificaron tres perfiles diferentes.",
+    text3: "Un estudio previo de las personas que asistirían al festival fue clave para averiguar qué tipo de propuesta realizar, tanto en web como en app. Pues las necesidades del usuario en los diferentes dispositivos era diferente. Es por eso que se identificaron tres perfiles diferentes.",
     img3: "./img/festival-dcu.webp",
     alt3: "árbol de funcionalidades de steez",
     imgaside: "./img/thumbnail-festival.webp",
@@ -89,46 +94,52 @@ const proyectos = [
     icono: "./img/lugar-iconotitulo.svg",
     alt: " ",
     titulo: "Fuera de lugar",
-    descripcion:
-      "¿Cómo sería acompañar a un artista musical a la hora de lanzar un disco? Desde los primeros singles, hasta el lanzamiento de álbum final. Pasando por el spotify canvas, redes sociales, diseño de vinilo y merchandasing. Así es Fuera de lugar, el disco de Kora bajo mi propia óptica.",
+    descripcion: "¿Cómo sería acompañar a un artista musical a la hora de lanzar un disco? Desde los primeros singles, hasta el lanzamiento de álbum final. Pasando por el spotify canvas, redes sociales, diseño de vinilo y merchandasing. Así es Fuera de lugar, el disco de Kora bajo mi propia óptica.",
 
     heading1: "Emociones a flor de piel",
-    text1:
-      "Al escuchar este disco y analizar sus letras profundamente, pude identificar emociones y sensaciones presentes: el pasado, lo inadecuado, el dolor o la sensibilidad. Basándome en un libro de Eva Heller sobre teoría del color, extraje la paleta gráfica.",
+    text1: "Al escuchar este disco y analizar sus letras profundamente, pude identificar emociones y sensaciones presentes: el pasado, lo inadecuado, el dolor o la sensibilidad. Basándome en un libro de Eva Heller sobre teoría del color, extraje la paleta gráfica.",
     img1: "./img/lugar-identidad.webp",
     alt1: "holaa",
 
     heading2: "Internet y música",
-    text2:
-      "La manera en la que hoy en día se realizan los lanzamientos musicales, me llevó a realizar la portada de los singles como adelantos del disco. Aportando a todo el conjunto una coherencia gráfica.",
+    text2: "La manera en la que hoy en día se realizan los lanzamientos musicales, me llevó a realizar la portada de los singles como adelantos del disco. Aportando a todo el conjunto una coherencia gráfica.",
     img2: "./img/lugar-spotify.webp",
     alt2: "holaa",
 
     heading3: "Lo tangible sigue siendo importante",
-    text3:
-      "Realicé diferentes tipos de merchandising, desde el vinilo, algunos posters, una camiseta y una bolsa de tela donde transportarlo todo :)",
+    text3: "Realicé diferentes tipos de merchandising, desde el vinilo, algunos posters, una camiseta y una bolsa de tela donde transportarlo todo :)",
     img3: "./img/lugar-merch.webp",
     alt3: "hola",
     imgaside: "./img/thumbnail-lugar.webp",
   },
 ];
 
-function mostrarProyecto() {
-  const contenedorProyecto = document.getElementById("proyecto");
-
-  const urlParams = new URLSearchParams(window.location.search);
-  const proyectoId = urlParams.get("id");
+function mostrarProyecto() {//2
+  const contenedorProyecto = document.getElementById("proyecto"); //3
+  const urlParams = new URLSearchParams(window.location.search); //4
+  const proyectoId = urlParams.get("id"); //5
 
   if (!proyectoId) {
+    // Esto sirve por si acaso no encuentra un id, que aparezca en pantalla un mensajito
     contenedorProyecto.innerHTML = `<p class="ProjectHeader-title"> Proyecto no proporcionado :( <br> Lo sentimos </p>`;
   }
-  const proyecto = proyectos.find((p) => p.id == proyectoId);
+
+  const proyecto = proyectos.find((p) => p.id == proyectoId); //6
+
+
+/* PROYECTOS pt2. Esta condicional evalúa si se encontró el proyecto, si lo encontró mostrará en pantalla su inner html (gracias a su id y sus template strings puede encontrar que valores imprimir en pantalla). Si no lo encontró, mostrará un mensaje tb con inner html donde lo comunicará
+
+1. Guardamos actualId que ha sido obtenido gracias a .indexOf 
+2. Proyecto previo: gracias a restar 1 al índice actualId || si ve que no hay proyecto anterior, toma el ultimo de la lista proyectos 
+3. Proyecto siguiente: lo va a obtener sumando 1 al indice actual || si no hay coge el primer proyecto de la lista proyectos
+4. Después pasamos a la creación del html que está estructurada en partes: la primera con el icono del proyecto, el título y el parrafo de descripción. Después tres secciones cada una explica una clave importante del proyecto y una imagen asociada a la explicación. Por último un aside por si quieres visitar el siguiente o el anterior proyecto*/
 
   if (proyecto) {
-    const actualId = proyectos.indexOf(proyecto);
-    const prevProyecto = proyectos[actualId - 1] || proyectos[proyectos.length - 1];
-    const nextProyecto = proyectos[actualId + 1] || proyectos[0];
+    const actualId = proyectos.indexOf(proyecto); //1
+    const prevProyecto = proyectos[actualId - 1] || proyectos[proyectos.length - 1]; //2
+    const nextProyecto = proyectos[actualId + 1] || proyectos[0]; //3
 
+    //4
     contenedorProyecto.innerHTML = `<article class="Project">
 
 
@@ -209,42 +220,34 @@ function mostrarProyecto() {
         </div>
 
         <div class="Section-imgContainer">
-          <img
-            class="SectionClave-img"
+          <img class="SectionClave-img"
             src="${proyecto.img3}"
             alt="${proyecto.alt3}"
-            loading="lazy"
-          />
+            loading="lazy"/>
         </div>
       </section>
 
-          <aside class="Aside" id="asideContainer">
-      <a href="projects.html?id=${prevProyecto.id}" title="Visita el proyecto ${prevProyecto.titulo}" class="Aside-container" id="Aside-prev">
-          <p class="Aside-text">Anterior Proyecto</p>
+      <aside class="Aside" id="asideContainer">
+      <a href="projects.html?id=${prevProyecto.id}" 
+      title="Visita el proyecto ${prevProyecto.titulo}" class="Aside-container" 
+      style="background-image: url(${prevProyecto.imgaside});>
+      <p class="Aside-text">Anterior Proyecto</p>
       </a>
 
-      <a href="projects.html?id=${nextProyecto.id}" title="Visita el proyecto ${nextProyecto.titulo}" class="Aside-container" id="Aside-next">          <p class="Aside-text">Siguiente Proyecto</p>
+      <a href="projects.html?id=${nextProyecto.id}" title="Visita el proyecto ${nextProyecto.titulo}" 
+      class="Aside-container" 
+      style="background-image: url(${nextProyecto.imgaside});> 
+      <p class="Aside-text">Siguiente Proyecto</p>
       </a>
+
     </aside>
-
     </article>`;
-
-    // Para seleccionar los Aside-containers' correspondientes
-    const asidePrev = document.getElementById("Aside-prev");
-    const asideNext = document.getElementById("Aside-next");
-
-    // Imágenes de fondo del Aside
-    if (asidePrev) {
-      asidePrev.style.backgroundImage = `url(${prevProyecto.imgaside})`;
-    }
-    if (asideNext) {
-      asideNext.style.backgroundImage = `url(${nextProyecto.imgaside})`;
-    }
   } else {
     contenedorProyecto.innerHTML = `<p class="ProjectHeader-title"> Proyecto no encontrado :( <br> Lo sentimos </p>`;
   }
 }
 
+// Para inicializar la función y que muestre el proyecto -cuando el DOM ya se ha cargado entonces lo muestra  
 document.addEventListener("DOMContentLoaded", () => {
   mostrarProyecto();
 });
